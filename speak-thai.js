3b0211c5c40efbdfd05671acfbb4d225137ccee5
Luke Hubbard <luke@codegent.com>
2014-11-05 23:33:10 +0700

    some changes to support opera

--- speak-thai.js
+++ speak-thai.js
@@ -1,9 +1,35 @@
 
-var speakThai = function(text){
-  var url = 'http://translate.google.co.th/translate_tts?ie=UTF-8&tl=th&q='+text; 
+var speakThai = function(text){ 
+  if(text.length > 100){
+    alert("Please select less than 100 characters to translate");
+    return; 
+  }
+  if(window.opera){
+    return speakThaiUsingIframeHack(text); 
+  }
+  var url = 'https://translate.google.co.th/translate_tts?ie=UTF-8&tl=th&q='+text; 
   var audio = new Audio(url);
+  audio.load(); 
   audio.play();
 };
+
+var hiddenIFrame = function(id){
+  var iframe = document.getElementById(id);
+  if(!iframe){
+    iframe = document.createElement('iframe');
+    iframe.id = id;
+    iframe.style.display = 'none'; 
+    document.body.appendChild(iframe); 
+  }
+  return iframe; 
+}
+
+var speakThaiUsingIframeHack = function(text){  
+  var iframe = hiddenIFrame('speak-thai-iframe-hack');
+  iframe.src = 'https://translate.google.co.th/translate_tts?ie=UTF-8&tl=th&q='+text; 
+};
+
+speakThai = speakThaiUsingIframeHack;
 
 chrome.contextMenus.create({
   "title" : "Speak Thai",
@@ -18,4 +44,3 @@ var handleRequest = function(request, sender, sendResponse){
 };
 
 chrome.extension.onRequest.addListener(handleRequest);
-
